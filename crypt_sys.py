82fb299e6a4d97f4671b185e508697f9b803c734
FuturizeHandgun <1996anoojpatel@gmail.com>
2016-10-01 09:40:43 -0700

    Updated the AES encryption with a try-catch in the option if statements, and also specify that you need a 16 char password since AES block length is 16.

--- crypt_sys.py
+++ crypt_sys.py
@@ -21,7 +21,7 @@ def operationMismatch(num):
 #Get args
 if (len(sys.argv)!=5):
 	sys.exit("ERROR: Need 4 arguments in the correct format.")
-script,option,inputfile,outputfile,password = sys.argv
+script,option,in_filename,out_filename,password = sys.argv
 
 
 
@@ -29,64 +29,55 @@ script,option,inputfile,outputfile,password = sys.argv
 # modified code from StackOverFlow AES boilerplate encryption code
 #/questions/16761458/
 
-from os import urandom
 from hashlib import md5
-
 from Crypto.Cipher import AES
+from Crypto import Random
 
 def derive_key_and_iv(password, salt, key_length, iv_length):
-    d = d_i = b'' 
+    d = d_i = ''
     while len(d) < key_length + iv_length:
-        d_i = md5(d_i + str.encode(password) + salt).digest()
+        d_i = md5(d_i + password + salt).digest()
         d += d_i
     return d[:key_length], d[key_length:key_length+iv_length]
-#encrypts a file based on the input and output file using password from input
-def encrypt(in_file, out_file, password, salt_header='', key_length=32):
+
+def encrypt(in_file, out_file, password, key_length=32):
     bs = AES.block_size
-    salt = urandom(bs - len(salt_header))
+    salt = Random.new().read(bs - len('Salted__'))
     key, iv = derive_key_and_iv(password, salt, key_length, bs)
     cipher = AES.new(key, AES.MODE_CBC, iv)
-    of = open(out_file,'wb')
-    of.write(str.encode(salt_header) + salt)
-    i_f = open(in_file,'rb')
+    out_file.write('Salted__' + salt)
     finished = False
     while not finished:
-        chunk = i_f.read(1024 * bs) 
+        chunk = in_file.read(1024 * bs)
         if len(chunk) == 0 or len(chunk) % bs != 0:
             padding_length = (bs - len(chunk) % bs) or bs
-            chunk += str.encode(
-                padding_length * chr(padding_length))
+            chunk += padding_length * chr(padding_length)
             finished = True
-        of.write(cipher.encrypt(chunk))
-    of.close()
-    i_f.close()
-#Decrypts a file based on the input files and passwords which is hashed with derive_key_and_iv
-def decrypt(in_file, out_file, password, salt_header='', key_length=32):
+        out_file.write(cipher.encrypt(chunk))
+
+def decrypt(in_file, out_file, password, key_length=32):
     bs = AES.block_size
-    i_f = open(in_file,'rb')
-    salt = i_f.read(bs)[len(salt_header):]
+    salt = in_file.read(bs)[len('Salted__'):]
     key, iv = derive_key_and_iv(password, salt, key_length, bs)
     cipher = AES.new(key, AES.MODE_CBC, iv)
     next_chunk = ''
     finished = False
-    of = open(out_file,'wb')
     while not finished:
-        chunk, next_chunk = next_chunk, cipher.decrypt(
-            i_f.read(1024 * bs))
+        chunk, next_chunk = next_chunk, cipher.decrypt(in_file.read(1024 * bs))
         if len(next_chunk) == 0:
-            padding_length = chunk[-1]  
+            padding_length = ord(chunk[-1])
             chunk = chunk[:-padding_length]
-            finished = True 
-            of.write(
-                bytes(x for x in chunk))
-    of.close()
-    i_f.close()
+            finished = True
+        out_file.write(chunk)
+
 if (option == '-e'):
-	encrypt(inputfile,outputfile,password)
+	with open(in_filename, 'rb') as in_file, open(out_filename, 'wb') as out_file:
+    		encrypt(in_file, out_file, password)
 	sys.exit(0)
 
 elif (option =='-d'):
-	decrypt(inputfile,outputfile,password)
+	with open(in_filename, 'rb') as in_file, open(out_filename, 'wb') as out_file:
+    		decrypt(in_file, out_file, password)
 	sys.exit(0)
 else:
 	exit("ERROR: Need to insert correct options, -e|-d")
