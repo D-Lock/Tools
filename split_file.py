8853c26740d7e738da6b0c4de99c5d94d2c250b6
Tremainne <thomsondj@hotmail.com>
2016-10-01 12:14:53 -0700

    Created splitter .py script

--- split_file.py
+++ split_file.py
@@ -20,20 +20,30 @@ def operationMismatch(num):
 		print ("ERROR: Invalid number of arguments (2 or 4 expected)\n")
 		sys.exit(0)
 
-if(len(sys.argv) == 2):
+if(len(sys.argv) <= 2):
 	printOptions()
 	sys.exit(0)
 
 elif(len(sys.argv) == 3):
 	inName,num = sys.argv[1:3]
-	(int) size = os.stat(inName)/num
+	
 	fid = 1
+	size = os.path.getsize(inName)
+	num = int(num)
+	size = int(size / num)
 
-	infile = open(inName)
+	infile = open(inName, 'rb')
 	strfile = inName + '_%d'
+	print ('Splitting file into {} parts'.format(num))
 	while (fid <= num):
-		f = open(strfile %fid, 'w')
+		print (fid)
+		infile.seek((fid-1) * size)
+		f = open(strfile %fid, 'wb+')
+		f.write(infile.read(size))
+		fid += 1;
+	if (f.read() != ''):
+		f.write(infile.read(size))
+	print ('Completed splitting files')
 		
-
 else:
 	operationMismatch(2)
