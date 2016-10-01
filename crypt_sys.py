ec90cf08ca3a3b4259b75c8a45beb9fffa95e126
FuturizeHandgun <1996anoojpatel@gmail.com>
2016-10-01 01:00:09 -0700

    Added options for user input with command line arguments

--- crypt_sys.py
+++ crypt_sys.py
@@ -2,4 +2,18 @@
 # Encryption/Decryption file for crypting files prior to upload
 
 import os
+#Create a command line arguement option
+# OPTIONS:
+def printOptions():
+	print "OPTIONS\n"
+	print "-help to print this menu"
+	print "Argument format: [-option] [inputfilename] [outputfilename] [password]\n"
+	print "-e : Encrypt a file with AES encryption\n"
+	print "-d : Decrypt a file with AES encryption\n"
+	
+def operationMismatch(num):
+	if (num == 0):
+		print "ERROR: File was not found\n"
+	elif (num == 1):
+		print "ERROR: File could not be encrypted\n"
 
